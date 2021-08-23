56c586378a6a5f503fb359d46ac537017af81878
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-08-23 10:26:23 -0500

    add Responsive

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
+  //screen larger than small
   desktop: {
     [theme.breakpoints.up("sm")]: {
       display: "none",
